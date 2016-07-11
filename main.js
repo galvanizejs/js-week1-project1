a3d995d9d58a504ae17031d97b7e681992920faa
galvanizejs <js@matthewleach.com>
2016-07-11 16:50:34 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+/*
+=======================================================
+
+** Week 1 - Project 1 **
+
+Below are a number of problems for you to solve
+using JS. The JS code can be written below each
+problem (if requested). 
+
+
+1-4: Basic
+=======================================================
+*/
+
+
+
+// 1. Create an alert that says: "The JS Code works!!!"
+//    Testing this HTML will not show the alert until you do #2
+
+
+
+
+// 2. The JS document is not connected yet to the HTML document. 
+//    Within the head of the document, add the 
+//    appropriate code to link the main.js file. 
+//    You will know you did this properly when you open the 
+//    index.html page in a browser and see the alert message.
+
+
+
+// 3. Display your name into the Console (and then find the console within the browser). 
+
+
+
+// 4. For this, you need to create a new Pen in CodePen.io to complete this assignment. 
+//    Display the name of one of your favorite people to the console.   
+//    Find the the name within the console in TWO locations: 
+//     - a) Find the console within the CodePen layout.
+//     - b) Find the console within the browser. 
+
+
